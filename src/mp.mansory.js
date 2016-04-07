cadb15c5516ee9ebfc14e53b2609c611494e73f4
jmlp131092 <jmlp131092@gmail.com>
2016-04-07 02:18:19 +0100

    first steps

--- src/mp.mansory.js
+++ src/mp.mansory.js
@@ -1,58 +1,88 @@
 (function ( $ ) {
-	$.fn.rbbMansory = function () {
 
-		return this.each(function () {
-			var _this = $(this);
+	$.fn.mpmansory = function ( options ) {
 
-			var items = _this.find('.rbb-mansory-item');
-			var wrappers = new Array();
-			console.log(items);
+		var settings = $.extend({
+			childrenClass: '',
+			breakpoints: {
+					lg: 4,
+					md: 4,
+					sm: 6,
+					xs: 12
+			},
+			onload: function ( items ) {
+				return true;
+			}
+			
+		}, options);
 
-			for (var i = 0; i<3; i++) {
-				var wrap = $('<div></div>').addClass('col-lg-4 col-md-4 co-sm-6 col-xs-12');
-				_this.append(wrap);
+
+		$.fn.initialize = function (columns, classStr) {
+			/*
+			 * @params [int] {columns} - number of grid columns
+			 * @params [string] {classStr} - the bootstrap column string
+			 * @description - creates the grid columns in wich the items will be distributed
+			 */
+			for (var i = 0; i<columns; i++) {
+				
+				var wrap = $('<div></div>').addClass(classStr);
+				$(this).append(wrap);
 				wrappers.push(wrap);
+			
 			}
 
+			return wrappers;
 
-			var columns = 3;
-			var rows = Math.ceil(items.length / columns);
+		}
+
+		$.fn.distributeItems = function ( wrappers, items ) {
+			/*
+			 * @params [Array] {wrappers} - the array containing the columns elements
+			 * @params [Array] {items} - the array containing items
+			 * @description - distribute the items through the columns - to the columns with lowest height
+			 */
 			var counter = 0;
 
-			for (var k = 0; k<rows; k++) {
-				if (items.length - 1 >= counter) {
-					wrappers[0].append(items[counter]);
-					counter++;
-				}
-				if (items.length - 1 >= counter) {
-					wrappers[1].append(items[counter]);
-					counter++;
-				}
-				if (items.length - 1 >= counter) {
-					wrappers[2].append(items[counter]);
-					counter++;
+			for (var k = 0; k<items.length; k++) {
+
+				var $heights = new Array();
+
+				for (var i = 0; i<wrappers.length; i++ ) {
+
+					//get the wrappers height
+					$heigths.push(wrappers[i].heigth());
+
 				}
 
-				
+				//get the wrapper with the lowest height
+				wrappers[$heights.indexOf(min($heights))].append(items[k]);
+	
 			}
 
-			/*
-			items.each(function (k) {
-				
-				wrappers[counter-decrement].append(items[k]);
+		}
 
-				if (k % match == 0 && k > 0) {
-					counter = 0;
-					decrement = 0;
-					
-				} else {
-					counter++;
-					mastch = 3;
-				}
-			});
-			*/
+
+		return this.each(function () {
+
+			var _this = $(this);
 			
+			var items = _this.children( (setttings.childrenClass != '' ? '.'+settings.childrenClass : 'div') ); //get the items
 
+			var columns = settings.breakpoints.lg / 12; //find number of columns
+
+			//build the bootstrap class string
+			var classStr = "col-lg-" + settings.breakpoints.lg + " col-md-"+settings.breakpoints.md + " col-sm-" + settings.breakpoints.sm + "col-xs-" + settings.breakpoints.xs;
+
+			var wrappers = $(this).initialize( columns ); //create columns
+
+			_this.distributeItems( wrappers, items); //apply mansory
+
+			if (settings.hasOwnProperty('onload')) {
+
+				//execute on load
+				settings.onload( items );
+
+			}
 
 		});
 	}
